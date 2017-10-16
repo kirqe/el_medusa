1e543c32a09f29204ee2625878b706b6c02ec0fb
kirqe <ikll7@yahoo.com>
2017-10-17 02:24:50 +0300

    ver

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ElMeduza.Mixfile do
   def project do
     [
       app: :el_meduza,
-      version: "0.1.1",
+      version: "0.1.2",
       elixir: "~> 1.5",
       start_permanent: Mix.env == :prod,
       package: package(),
